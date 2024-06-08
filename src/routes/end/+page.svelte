281fe9bd4f4eec140bd7367f94adc9a3dfbbc34d
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-06-09 05:14:41 +0900

    feat: end 페이지 수정

--- src/routes/end/+page.svelte
+++ src/routes/end/+page.svelte
@@ -1,9 +1,14 @@
+<script>
+	import image_q21 from '$lib/images/q21.jpeg';
+</script>
+
 <h1 class="visually-hidden">최종 페이지</h1>
 
 <div>
 	<p>이제 엘리베이터를 타고</p>
 	<p>5층을 눌러 보자.</p>
 </div>
+<img class="image" src={image_q21} alt="엘리베이터" />
 
 <style>
 	div {
@@ -12,5 +17,9 @@
 	p {
 		font-size: x-large;
 		margin: 10px auto;
+	}
+	img {
+		width: 80%;
+		margin: 20px auto;
 	}
 </style>
