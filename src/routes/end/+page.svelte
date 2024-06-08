5500fd2324103cabbd58d84241793b0a586fd2e2
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-06-09 04:26:30 +0900

    feat: 최종 페이지 추가

---
+++ src/routes/end/+page.svelte
@@ -0,0 +1,16 @@
+<h1 class="visually-hidden">최종 페이지</h1>
+
+<div>
+	<p>이제 엘리베이터를 타고</p>
+	<p>5층을 눌러 보자.</p>
+</div>
+
+<style>
+	div {
+		margin: 50px auto;
+	}
+	p {
+		font-size: x-large;
+		margin: 10px auto;
+	}
+</style>
