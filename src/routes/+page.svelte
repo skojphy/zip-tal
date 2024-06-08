7d707a1dbddf72f300c5c425525d46800d9c85a5
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-06-09 01:58:55 +0900

    feat: 메인페이지 수정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-
-<h1>우당탕탕 화곡동</h1>
+<h1>시작 위치를 선택하세요.</h1>
+
+<div>
+	<a href="./station">까치산역에서 시작하기</a>
+	<a href="./market">화곡중앙시장에서 시작하기</a>
+</div>
+
+<style>
+	h1 {
+		font-size: x-large;
+		margin: 50px;
+	}
+	div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	a {
+		width: 70%;
+		height: 50px;
+		background-color: var(--color-theme-1);
+		color: var(--color-bg-0);
+		margin: 20px;
+		text-align: center;
+		line-height: 50px;
+	}
+</style>
