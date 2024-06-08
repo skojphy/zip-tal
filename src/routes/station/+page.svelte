fed24c179792f9b35fc7f97c15a6820fdb254cb0
서혜연 <seohyeyeon@seohyeyeon-ui-MacBookAir.local>
2024-06-09 01:59:48 +0900

    feat: 첫번째 문제 추가

---
+++ src/routes/station/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import image_q1 from '$lib/images/q1.png';
+	let currentNumber = 0;
+</script>
+
+<h1 class="visually-hidden">까치산역</h1>
+
+<p>
+	<span class="text">와. 까치산역 드디어 다 왔다.</span>
+	<span class="text">진짜 머네.</span>
+</p>
+
+<img class="image" src={image_q1} alt="문제1" />
+
+<input class="text-input" type="text" />
+<button class="button">확인</button>
+
+<style>
+	p {
+		margin: 20px;
+	}
+	.text {
+		display: block;
+		font-size: 1.5rem;
+		margin: 10px;
+	}
+	.image {
+		width: 80%;
+	}
+	.text-input {
+		font-size: 1.5rem;
+		height: 3rem;
+		margin: 1rem;
+		width: 80%;
+		border: 1px solid var(--color-theme-1);
+		border-radius: 7px;
+		text-align: center;
+	}
+	.button {
+		width: 40%;
+		height: 3rem;
+		background-color: var(--color-theme-1);
+		border-radius: 7px;
+		color: white;
+		border: none;
+	}
+</style>
